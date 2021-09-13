55b4877d26c01a11f05e474221dbf2a4e472056f
Sebastian Silva <sebastian@fuentelibre.org>
2021-09-13 02:24:58 -0500

    Try fix for motion

--- src/components/TreeList.vue
+++ src/components/TreeList.vue
@@ -64,10 +64,10 @@ export default defineComponent({
           let lastLocation = $store.state.trees.location_data [$store.state.trees.location_data.length - 1]
           const north_bearing = bearing(row.location_latitude, row.location_longitude,
                         lastLocation.coords.latitude, lastLocation.coords.longitude) - 1
-          if (Array.isArray($store.state.motion_data)) {
-            console.log(north_bearing, $store.state.motion_data[$store.state.motion_data.length -1])
+          if (Array.isArray($store.state.trees.motion_data)) {
+            console.log(north_bearing, $store.state.motion_data[$store.state.trees.motion_data.length -1])
             console.log("Adjusting for motion")
-            return north_bearing + $store.state.motion_data.slice(-1) - 180
+            return north_bearing + $store.state.trees.motion_data.slice(-1) - 180
           }
           return north_bearing
         }
