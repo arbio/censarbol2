23b3be8bda28202604c803c873bc85a1a87e1808
Sebastian Silva <sebastian@fuentelibre.org>
2021-09-23 10:04:41 -0500

    Change OAuth Client ID for permissions

--- src/boot/vue-gapi.js
+++ src/boot/vue-gapi.js
@@ -3,7 +3,7 @@ import VueGapi from 'vue-gapi'
 export default ({ app, router, store }) => {
     app.use(VueGapi, {
                 apiKey: 'AIzaSyAtQDpFQtP39ZwgELTJ8ue_JSDcqGn7o5g',
-                clientId: '596878713214-id2ucbv5r9996o55kuqlvi6bgovqdv1b.apps.googleusercontent.com',
+                clientId: '596878713214-51gba9vmee64tua02965bpe5ftup03sv.apps.googleusercontent.com',
                 discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
                 scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
                 })
