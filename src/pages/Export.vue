c1aeaceaec353272bfa08ad26308f6762232da92
Sebastian Silva <sebastian@fuentelibre.org>
2021-09-23 09:53:19 -0500

    Add missing files

---
+++ src/pages/Export.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex column items-center">
+    <h1>Exportar</h1>
+    <button @click="uploadFiles">Enviar a Google Drive</button>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, getCurrentInstance } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const _instance = getCurrentInstance()
+    const $gapi = _instance.appContext.app.config.globalProperties.$gapi
+    console.log(_instance.appContext.app.config.globalProperties)
+
+    function uploadFiles() {
+        $gapi.login().then(({ currentUser, gapi, hasGrantedScopes }) => {
+          console.log({ currentUser, gapi, hasGrantedScopes })
+        })
+    }
+    return { uploadFiles }
+  }
+})
+</script>
