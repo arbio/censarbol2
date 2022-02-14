5b81e39cb4d0779e9280b2554892b9973fd83c62
Sebastian Silva <sebastian@fuentelibre.org>
2022-02-14 10:40:41 -0500

    "Individuo"->"Código"

--- src/store/model.js
+++ src/store/model.js
@@ -14,7 +14,7 @@ export default {
     [
         { name: 'date', label: 'Fecha', hint: 'Día de Censo', type: 'date', 
           rules: ['date'], mask: 'date' },
-        { name: 'name', label: 'Individuo', hint: 'Identificador único',
+        { name: 'name', label: 'Código', hint: 'Identificador único',
           rules: [value=>!!value || 'Campo necesario'] },
         { name: 'location_latitude', label: 'Latitud', hint: 'GPS', recalc:true },
         { name: 'location_longitude', label: 'Longitud', hint: 'GPS', recalc:true },
